3a8d1ade95abba3e0d0a8a3326b5ad87675071ce
Menuja <it25102298@my.sliit.lk>
2026-05-01 11:37:23 +0530

    Add files via upload

---
+++ src/test/java/com/student/Foodly/FoodlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.student.Foodly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
